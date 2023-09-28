5c9f6165f40f5aa95a72645327644394adb8b522
SonOfLope <jonathanlopez@hotmail.ca>
2023-09-28 12:37:30 -0400

    remove assignemnts

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,15 +4,3 @@ resource "azurerm_role_assignment" "acrpull_role" {
   principal_id         = azurerm_kubernetes_cluster.jolopezcluster.kubelet_identity[0].object_id
   depends_on = [azurerm_kubernetes_cluster.jolopezcluster]
 }
-
-resource "azurerm_role_assignment" "acrpush_role" {
-  principal_id            = "bb78d222-8e69-4abf-a682-9e906e33ded4"
-  role_definition_name    = "AcrPush"  
-  scope                   = azurerm_container_registry.acr.id
-}
-
-resource "azurerm_role_assignment" "contributor_role" {
-  principal_id         = "bb78d222-8e69-4abf-a682-9e906e33ded4"
-  role_definition_name = "Contributor"
-  scope                = "/subscriptions/7e349100-77f0-4750-9bff-354325c60926"
-}
